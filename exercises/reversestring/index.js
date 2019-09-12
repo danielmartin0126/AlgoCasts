c06136a632a760215ca7a8c85971d1b21ca04059
danielmartin0126 <danielmartin0126@gmail.com>
2019-09-12 15:23:41 -0400

    reversestring complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,11 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reverse = []
+    let arr = str.split("")
+    arr.map(letter => reverse.unshift(letter))
+    return reverse.join("")
+}
 
 module.exports = reverse;
