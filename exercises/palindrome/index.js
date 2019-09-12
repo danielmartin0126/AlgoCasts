ac955e9564ff02c0b5d38e6305f321cff7fb3763
danielmartin0126 <danielmartin0126@gmail.com>
2019-09-12 16:59:58 -0400

    more palindrome solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,9 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//     let reverse = str.split("").reduce((rev,char) => char + rev, "")
+//     return str === reverse
+// }
+
 function palindrome(str) {
-    let reverse = str.split("").reduce((rev,char) => char + rev, "")
-    return str === reverse
+   return str.split("").every((char, i) => char === str[str.length - i - 1])
 }
 
 module.exports = palindrome;
